7b12988fee90daaf566e9b4293e9eb6f7cef0375
Matte <mattias4@kth.se>
2017-08-15 23:56:02 +0200

    Now using the router api to push new HTML5 state

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,15 +3,25 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	goToStore(event) {
+		event.preventDefault();
+		const storeId = this.storeInput.value;
+		this.context.router.transitionTo(`/store/${storeId}`);
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
 				<h2>Please enter store name</h2>
-				<input type="text" required placeholder="store name" defaultValue={getFunName()}/>
+				<input type="text" required placeholder="store name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
 				<button type="submit">Visit Store --></button>
 			</form>
 		);
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
