f0facba45c1a0c8a951ad3bfcee9222573b40d14
Matte <mattias4@kth.se>
2017-08-16 16:26:44 +0200

    Now showing order items

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -2,21 +2,35 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
+	renderOrderItem(key) {
+		const fish = this.props.fishes[key];
+		const amount = this.props.order[key];
+		return (
+			<li key={key}>
+				{amount}lbs of {fish.name}
+				<span className="price">{formatPrice(amount * fish.price)}</span>
+			</li>
+		)
+	}
+
 	render() {
 		const total = Object.keys(this.props.order).reduce((acc, key) => {
 			return acc + this.props.fishes[key].price * this.props.order[key];
 		}, 0);
 		return (
-			<div>
-				<h2>My Order</h2>
-				{
-					Object
-						.keys(this.props.order)
-						.map(key => {
-							return <div>{this.props.fishes[key].name}</div>;
-						})
-				}
-				<p>Total: {formatPrice(total)}</p>
+			<div className="order-wrapp">
+				<h2>Your Order</h2>
+				<ul className="order">
+					{
+						Object
+							.keys(this.props.order)
+							.map(this.renderOrderItem.bind(this))
+					}
+					<li className="total">
+						<strong>Total:</strong>
+						{formatPrice(total)}
+					</li>
+				</ul>
 			</div>
 		)
 	}
