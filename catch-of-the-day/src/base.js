7c16428c8a77ff2118b5cdb311549f580c2d308f
Matte <mattias4@kth.se>
2017-08-17 02:15:05 +0200

    Now using re-base for syncing state with firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBwY13LJz1K2YjQ5lwwr11IJ9mwF3sKR4o",
+    authDomain: "catch-of-the-day-4cae1.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-4cae1.firebaseio.com"
+});
+
+export default base;
